07289e0e9f704e4e8e256446ef0f2216bd2abd66
Razan <razan.ashhab.00@gmail.com>
2023-01-13 14:01:45 +0200

    commit changes

--- scripts/script.js
+++ scripts/script.js
@@ -2,62 +2,130 @@ const profile = document.querySelector(".profile");
 const profileEditButton = profile.querySelector(".profile__edit");
 const profileName = profile.querySelector(".profile__name");
 const profileDescription = profile.querySelector(".profile__description");
-const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close-button");
-const nameText = popup.querySelector("#name");
-const aboutmeText = popup.querySelector("#aboutme");
-const saveButton = popup.querySelector(".form__button");
-const form = popup.querySelector("form");
+const editProfile = document.querySelector("#editProfile");
+const closeEditProfileBtn = editProfile.querySelector("#closeEditProfile");
+const nameText = editProfile.querySelector("#name");
+const aboutmeText = editProfile.querySelector("#aboutme");
+const saveButton = editProfile.querySelector(".form__button");
+const editProfileForm = editProfile.querySelector("#editProfileForm");
 
-DisplayCard();
+const cardCreationButton = profile.querySelector(".profile__add-button");
+const createCard = document.querySelector("#createCard");
+const closeCreateCardBtn = createCard.querySelector("#closeCreateCard");
+const title = createCard.querySelector("#title");
+const imagelink = createCard.querySelector("#imagelink");
+const createCardForm = createCard.querySelector("#createCardForm");
 
-function DisplayCard() {
-    const element = document.querySelector(".element");
-    const template = document.querySelector("#template");
-    const images = [
-        "yosemite-valley.jpg",
-        "lake-louise.png",
-        "bald-mountains.jpg",
-        "latemar.jpg",
-        "vanoise-national-park.jpg",
-        "lago-di-braies.jpg",
-    ];
-    const imagenames = [
-        "Yosemite Valley",
-        "Lake Louise",
-        "Bald Mountains",
-        "Latemar",
-        "Vanoise National Park",
-        "Lago di Braies",
-    ];
-    for (let i = 0; i < images.length; i++) {
-        const clone = template.content.cloneNode(true);
-        const cardImage = clone.querySelector(".card__image");
-        const cardParagraph = clone.querySelector(".card__paragraph");
-        cardImage.setAttribute("src", `./images/${images[i]}`);
-        cardImage.setAttribute("alt", `image of ${imagenames[i]}`);
-        cardParagraph.textContent = imagenames[i];
-        element.appendChild(clone);
-    }
+const likeButton = document.querySelector(".card__button");
+const picturePopup = document.querySelector("#picturePopup");
+const closePicturePopup = picturePopup.querySelector("#closePicturePopup");
+
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+];
+const element = document.querySelector(".element");
+const template = document.querySelector("#template");
+
+function addCard(cardLink, cardName) {
+    const clone = template.content.cloneNode(true);
+    const cardImage = clone.querySelector(".card__image");
+    const cardParagraph = clone.querySelector(".card__paragraph");
+    cardImage.setAttribute("src", `${cardLink}`);
+    cardImage.setAttribute("alt", `image of ${cardName}`);
+    clone
+        .querySelector(".card__button")
+        .addEventListener("click", function(evt) {
+            evt.target.classList.toggle("card__button_active");
+        });
+    clone
+        .querySelector(".card__delete-button")
+        .addEventListener("click", function(evt) {
+            evt.target.parentElement.remove();
+        });
+    clone.querySelector(".card__image").addEventListener("click", function(evt) {
+        picturePopup.classList.toggle("popup_opened");
+        const popupImage = picturePopup.querySelector(".popup__image");
+        popupImage.setAttribute("src", `${evt.target.getAttribute("src")}`);
+        popupImage.setAttribute(
+            "alt",
+            `image of ${evt.target.getAttribute("alt")}`
+        );
+        const popupImageDescription = picturePopup.querySelector(
+            ".popup__image-description"
+        );
+        popupImageDescription.textContent =
+            evt.target.nextElementSibling.firstElementChild.innerText;
+    });
+    cardParagraph.textContent = cardName;
+    return clone;
 }
 
-function showPopup() {
+initialCards.forEach((card) => {
+    const clone = addCard(card.link, card.name);
+    element.appendChild(clone);
+});
+
+function showEditProfile() {
     nameText.value = profileName.textContent;
     aboutmeText.value = profileDescription.textContent;
-    popup.classList.add("popup_opened");
+    editProfile.classList.add("popup_opened");
 }
 
-function closePopup() {
-    popup.classList.remove("popup_opened");
+function closeEditProfile() {
+    editProfile.classList.remove("popup_opened");
 }
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameText.value;
     profileDescription.textContent = aboutmeText.value;
-    closePopup();
+    closeEditProfile();
 }
 
-profileEditButton.addEventListener("click", showPopup);
-closeButton.addEventListener("click", closePopup);
-form.addEventListener("submit", handleProfileFormSubmit);
+function createCardFormToggle() {
+    createCard.classList.toggle("popup_opened");
+}
+
+function handleCreateCardFormSubmit(evt) {
+    evt.preventDefault();
+    const addedTitle = title.value;
+    const addedImage = imagelink.value;
+    const clone = addCard(addedImage, addedTitle);
+    element.prepend(clone);
+    createCardFormToggle();
+}
+
+function createPictureToggle() {
+    picturePopup.classList.toggle("popup_opened");
+}
+
+profileEditButton.addEventListener("click", showEditProfile);
+closeEditProfileBtn.addEventListener("click", closeEditProfile);
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+cardCreationButton.addEventListener("click", createCardFormToggle);
+closeCreateCardBtn.addEventListener("click", createCardFormToggle);
+createCardForm.addEventListener("submit", handleCreateCardFormSubmit);
+closePicturePopup.addEventListener("click", createPictureToggle);
